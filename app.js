08a5f071a0b8824d930cb1a3f53f1c43c3dc3eac
Jyoti-Adhikari <jyotiadhikari770@gmail.com>
2025-05-11 16:45:05 +0545

    Delete & Edit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const path = require('path');
+const methodOverride = require('method-override');
 
 main().then(() => {
     console.log('Connected to MongoDB');
@@ -23,6 +24,7 @@ app.listen(8080, () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 //basic api
 app.get('/', (req, res) => {
@@ -37,10 +39,79 @@ app.get("/listings",async (req, res) => {
 }
 );
 
+//route to create a new listing
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new");
+}
+);
+
+
 //when clicked in any place name
 app.get("/listings/:id", async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show", { listing });
+}
+);
+
+// Create new route after clicking submit button
+app.post("/listings", async (req, res) => {
+    const { title, price, description, location, country, image } = req.body;
+
+    const newListing = new Listing({
+        title,
+        price,
+        description,
+        image: {
+            url: image || "https://images.unsplash.com/photo-1630997065202-afb8c444da0a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            filename: "default"  // Optional: you can use a better name or extract from the URL
+        },
+        location,
+        country
+    });
+
+    await newListing.save();
+    res.redirect(`/listings/`);
+});
+
+
+//Edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", { listing });
+}
+);
+
+
+// Update listing – handles form submission from the edit page
+app.put("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, price, description, location, country, image } = req.body;
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+        id,
+        {
+            title,
+            price,
+            description,
+            image: {
+                url: image,
+                filename: "updated" // optional
+            },
+            location,
+            country
+        },
+        { new: true }
+    );
+
+    res.redirect(`/listings/`);
+});
+
+//to delete a listing
+app.delete("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect(`/listings/`);
 }
 );
